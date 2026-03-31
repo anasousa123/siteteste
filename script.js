52abb2fd04a260bece42e3d58a9e71d43d125aa9
anasousa123 <ana.7410062@aluno.mg.gov.br>
2026-03-31 16:21:34 +0000

    mold1

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-function clicou() {
-  document.getElementById("mensagem").innerText = "Funcionou!";
+const planetas = [
+    {
+        nome: "Marte",
+        img: "https://via.placeholder.com/300/ff0000",
+        distancia: 225
+    },
+    {
+        nome: "Júpiter",
+        img: "https://via.placeholder.com/300/f0a500",
+        distancia: 778
+    },
+    {
+        nome: "Saturno",
+        img: "https://via.placeholder.com/300/cccc99",
+        distancia: 1429
+    }
+];
+
+let index = 0;
+
+function trocarPlaneta() {
+    index = (index + 1) % planetas.length;
+    document.getElementById("planeta").src = planetas[index].img;
+}
+
+function adicionarPlaneta() {
+    const tabela = document.getElementById("tabela");
+
+    const p = planetas[Math.floor(Math.random() * planetas.length)];
+
+    const linha = document.createElement("tr");
+
+    const nome = document.createElement("td");
+    nome.textContent = p.nome;
+
+    const distancia = document.createElement("td");
+    distancia.textContent = p.distancia;
+
+    linha.appendChild(nome);
+    linha.appendChild(distancia);
+
+    tabela.appendChild(linha);
 }
